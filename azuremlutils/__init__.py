3bc76da2883bcfe8b8e6968426c219ab5b455989
jerbly <jeremyblythe@gmail.com>
2021-01-17 15:00:17 -0500

    Formatting check

---
+++ azuremlutils/__init__.py
@@ -0,0 +1,16 @@
+class AzureRunLogCallback(fastai.Callback):
+    "Log losses and metrics to Azure"
+
+    def __init__(self, run_context):
+        self.run_context = run_context
+
+    def after_epoch(self):
+        # log metrics
+        for n, v in zip(self.learn.recorder.metric_names, self.learn.recorder.log):
+            if n not in ["epoch", "time"]:
+                if isinstance(v, dict):
+                    for km, vm in v.items():
+                        self.run_context.log(f"{n}_{km}", vm)
+                else:
+                    self.run_context.log(f"{n}", v)
+        return True
